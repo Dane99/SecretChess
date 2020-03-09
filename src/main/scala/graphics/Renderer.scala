61d6eb2c5a23a410b75f781362f63a06411f0589
Dane Claessen <danec99@gmail.com>
2020-03-09 15:09:25 -0500

    added piece renderer

--- src/main/scala/graphics/Renderer.scala
+++ src/main/scala/graphics/Renderer.scala
@@ -1,17 +1,16 @@
 package graphics
-import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
 import org.scalajs.dom
-import org.scalajs.dom.{Document, EventTarget, html}
-import org.scalajs.dom.raw.{HTMLImageElement, MouseEvent}
+import org.scalajs.dom.html
+import org.scalajs.dom.raw.HTMLImageElement
 
 import scala.scalajs.js
-import scala.util.Random
+import scala.scalajs.js.annotation.JSExportTopLevel
 
 object Renderer {
   class Image(src: String) {
     private var ready: Boolean = false
 
-    val element = dom.document.createElement("img").asInstanceOf[HTMLImageElement]
+    val element: HTMLImageElement = dom.document.createElement("img").asInstanceOf[HTMLImageElement]
     element.onload = (e: dom.Event) => ready = true
     element.src = src
 
@@ -23,54 +22,107 @@ object Renderer {
     val h = canvas.height
     val w = canvas.width
 
-    val verticalBlockSize = h/8.0;
-    val horizontalBlockSize = w/8.0;
+    val verticalBlockSize = h/8.0
+    val horizontalBlockSize = w/8.0
 
     val ctx = canvas.getContext("2d")
       .asInstanceOf[dom.CanvasRenderingContext2D]
 
-    var selectedPieceX = -1;
-    var selectedPieceY = -1;
+    var selectedPieceX = -1
+    var selectedPieceY = -1
 
-    canvas.onmousemove = { (e: dom.MouseEvent) =>
+    //canvas.onmousemove = { (e: dom.MouseEvent) =>
       //println("X: " + e.pageX)
       //println("Y: " + e.pageY)
-    }
-    canvas.onmousedown = { (e: dom.MouseEvent) =>
-      val sx = math.floor(e.pageX / horizontalBlockSize).toInt
-      val sy = math.floor(e.pageY / verticalBlockSize).toInt
-      if(sx >= 0 && sx <= 8 && sy >= 0 && sy <= 8) {
-        selectedPieceX = sx
-        selectedPieceY = sy
+    //}
+    canvas.onmousedown = { e: dom.MouseEvent =>
+      val nx = math.floor(e.pageX / horizontalBlockSize).toInt
+      val ny = math.floor(e.pageY / verticalBlockSize).toInt
+      if(nx >= 0 && nx <= 8 && ny >= 0 && ny <= 8) {
+        if(selectedPieceX != -1 && selectedPieceY != -1){
+          //if(validMove(selectedPieceX, selectedPieceY, nx, ny)) {
+            // move piece from selectedPieceX and selectedPieceY to nx and ny.
+          //}
+        }
+
+        selectedPieceX = nx
+        selectedPieceY = ny
       }
       else {
-        selectedPieceX = -1;
-        selectedPieceY = -1;
+        selectedPieceX = -1
+        selectedPieceY = -1
       }
+
+
       println("Selected piece X:" + selectedPieceX + " Y:" + selectedPieceY)
     }
 
-    def clear() = {
+    def clear(): Unit = {
       ctx.fillStyle = "black"
       ctx.fillRect(0, 0, h, w)
     }
 
     val bgImage = new Image("./src/resources/images/ChessPiecesArray.png")
 
-    def drawBoard() = {
+    def drawQueen(x: Double, y: Double, white: Boolean): Unit ={
+      ctx.drawImage(bgImage.element, 0, if(white) 60 else 0, 60, 60,
+        x, y, 60 ,60)
+    }
+
+    def drawKing(x: Double, y: Double, white: Boolean): Unit ={
+      ctx.drawImage(bgImage.element, 60, if(white) 60 else 0, 60, 60,
+        x, y, 60 ,60)
+    }
+
+    def drawRook(x: Double, y: Double, white: Boolean): Unit ={
+      ctx.drawImage(bgImage.element, 120, if(white) 60 else 0, 60, 60,
+        x, y, 60 ,60)
+    }
+
+    def drawKnight(x: Double, y: Double, white: Boolean): Unit ={
+      ctx.drawImage(bgImage.element, 180, if(white) 60 else 0, 60, 60,
+        x, y, 60 ,60)
+    }
+
+    def drawBishop(x: Double, y: Double, white: Boolean): Unit ={
+      ctx.drawImage(bgImage.element, 240, if(white) 60 else 0, 60, 60,
+        x, y, 60 ,60)
+    }
+
+    def drawPawn(x: Double, y: Double, white: Boolean): Unit ={
+      ctx.drawImage(bgImage.element, 300, if(white) 60 else 0, 60, 60,
+        x, y, 60 ,60)
+    }
+
+
+    def drawBoard(): Unit = {
       for(i <- 0 until 8) {
         for(j <- 0 until 8) {
-          if((i+j)%2==1) ctx.fillStyle = s"rgb(0, 0, 0)"
+          if((i+j)%2==1) ctx.fillStyle = s"rgb(50, 100, 50)"
           else ctx.fillStyle = s"rgb(255, 255, 255)"
           ctx.fillRect(i*horizontalBlockSize, j*verticalBlockSize, verticalBlockSize, horizontalBlockSize)
         }
       }
     }
 
-    def render() {
+    def updateBoardPieces() {
+      val random = new scala.util.Random
       if (bgImage.isReady) {
-        ctx.drawImage(bgImage.element, 0, 0, 60, 60,
-          0, 0, 60 ,60)
+        for(x <- 0 until 8) {
+          for (y <- 0 until 8) {
+            val rnd = new scala.util.Random
+            val p = rnd.nextInt(6)
+            val w = rnd.nextInt(2) == 1
+            p match {
+              case 0 => drawQueen(x*horizontalBlockSize, y*verticalBlockSize, w)
+              case 1 => drawKing(x*horizontalBlockSize, y*verticalBlockSize, w)
+              case 2 =>  drawRook(x*horizontalBlockSize, y*verticalBlockSize, w)
+              case 3 =>  drawKnight(x*horizontalBlockSize, y*verticalBlockSize, w)
+              case 4 =>  drawBishop(x*horizontalBlockSize, y*verticalBlockSize, w)
+              case 5 => drawPawn(x*horizontalBlockSize, y*verticalBlockSize, w)
+            }
+          }
+        }
       }
     }
 
@@ -82,7 +134,7 @@ object Renderer {
 
       //update(delta / 1000)
       drawBoard()
-      render()
+      updateBoardPieces()
 
       prev = now
     }
